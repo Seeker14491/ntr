545ce512e952adc034ecd0af7ade1abd7500665e
Seeker14491 <seeker14491@gmail.com>
2015-09-19 04:04:54 -0500

    restructure; implement listening thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,96 @@
 #![allow(dead_code)]
+#![allow(unused_variables)]
+
+#![feature(read_exact)]
 
 extern crate byteorder;
 
 use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::thread;
 use byteorder::{ByteOrder, LittleEndian};
 
 #[derive(Debug)]
-pub struct NtrStream {
+pub struct Ntr {
     tcp_stream: TcpStream,
-    current_seq: u32,
-    is_heartbeat_sendable: bool,
+    current_seq: AtomicUsize,
+    is_heartbeat_sendable: AtomicBool,
 }
-impl NtrStream {
-    pub fn connect(address: &str) -> io::Result<Self> {
-        TcpStream::connect(&(address.to_owned() + ":8000") as &str)
-            .map(|x| NtrStream {
-                tcp_stream: x,
-                current_seq: 1000,
-                is_heartbeat_sendable: true,
-            })
+impl Ntr {
+    pub fn connect(address: &str) -> io::Result<Arc<Self>> {
+        let tcp_stream = try!(TcpStream::connect(&(address.to_owned() + ":8000") as &str));
+
+        let ntr = Arc::new(Ntr {
+            tcp_stream: tcp_stream,
+            current_seq: AtomicUsize::new(1000),
+            is_heartbeat_sendable: AtomicBool::new(true),
+        });
+
+        // spawn receiver thread
+        {
+            let ntr = ntr.clone();
+            thread::spawn(move || {
+                let mut ntr_stream = NtrStream::new(&ntr);
+                let mut buf = [0u8; 84];
+                loop {
+                    if let Err(_) = ntr_stream.tcp_stream().read_exact(&mut buf) {
+                        break;
+                    }
+                    let seq = LittleEndian::read_u32(&buf[4..8]);
+                    let cmd = LittleEndian::read_u32(&buf[12..16]);
+                    let data_len = LittleEndian::read_u32(&buf[80..84]) as usize;
+
+                    if data_len != 0 {
+                        let mut data_buf = Vec::with_capacity(data_len);
+                        ntr_stream.tcp_stream().read_exact(&mut data_buf[0..data_len]).unwrap();
+                        if cmd == 0 {
+                            ntr_stream.set_heartbeat_sendable(true);
+                        } else if cmd == 9 {
+                            // TODO; handle read mem
+                        }
+                    }
+                }
+            });
+        }
+
+        // spawn heartbeat thread
+        {
+            let ntr = ntr.clone();
+            thread::spawn(move || {
+                let ntr_stream = NtrStream::new(&ntr);
+                // TODO
+            });
+        }
+
+        Ok(ntr)
     }
 
     pub fn disconnect(self) {}
+}
 
-    pub fn send_packet(&mut self, packet_type: u32, cmd: u32, args: &Vec<u32>, data_len: u32) -> io::Result<usize> {
+#[derive(Debug)]
+struct NtrStream<'a> {
+    tcp_stream: TcpStream,
+    current_seq: &'a AtomicUsize,
+    is_heartbeat_sendable: &'a AtomicBool,
+}
+impl<'a> NtrStream<'a> {
+    fn new(ntr: &'a Ntr) -> Self {
+        NtrStream {
+            tcp_stream: ntr.tcp_stream.try_clone().unwrap(),
+            current_seq: &ntr.current_seq,
+            is_heartbeat_sendable: &ntr.is_heartbeat_sendable,
+        }
+    }
+
+    fn send_packet(&mut self, packet_type: u32, cmd: u32, args: &Vec<u32>, data_len: u32) -> io::Result<usize> {
         let mut buf = [0u8; 84];
 
         LittleEndian::write_u32(&mut buf[0..4], 0x12345678);
-        LittleEndian::write_u32(&mut buf[4..8], self.current_seq);
+        LittleEndian::write_u32(&mut buf[4..8], self.current_seq.fetch_add(1000, Ordering::SeqCst) as u32);
         LittleEndian::write_u32(&mut buf[8..12], packet_type);
         LittleEndian::write_u32(&mut buf[12..16], cmd);
         for i in 0..16 {
@@ -37,15 +98,14 @@ impl NtrStream {
         }
         LittleEndian::write_u32(&mut buf[80..84], data_len);
 
-        self.current_seq += 1000;
         self.tcp_stream.write(&buf)
     }
 
-    pub fn send_read_mem_packet(&mut self, addr: u32, size: u32, pid: u32) -> io::Result<usize> {
+    fn send_read_mem_packet(&mut self, addr: u32, size: u32, pid: u32) -> io::Result<usize> {
         self.send_empty_packet(9, pid, addr, size)
     }
 
-    pub fn send_write_mem_packet(&mut self, addr: u32, pid: u32, buf: &Vec<u8>) -> io::Result<usize> {
+    fn send_write_mem_packet(&mut self, addr: u32, pid: u32, buf: &Vec<u8>) -> io::Result<usize> {
         let args = &mut vec![0u32; 16];
         args[0] = pid;
         args[1] = addr;
@@ -54,19 +114,19 @@ impl NtrStream {
             .and(self.tcp_stream.write(buf))
     }
 
-    pub fn send_heartbeat_packet(&mut self) -> io::Result<usize> {
+    fn send_heartbeat_packet(&mut self) -> io::Result<usize> {
         self.send_packet(0, 0, &vec![0u32; 16], 0)
     }
 
-    pub fn send_hello_packet(&mut self) -> io::Result<usize> {
+    fn send_hello_packet(&mut self) -> io::Result<usize> {
         self.send_packet(0, 3, &vec![0u32; 16], 0)
     }
 
-    pub fn send_reload_packet(&mut self) -> io::Result<usize> {
+    fn send_reload_packet(&mut self) -> io::Result<usize> {
         self.send_packet(0, 4, &vec![0u32; 16], 0)
     }
 
-    pub fn send_empty_packet(&mut self, cmd: u32, arg0: u32, arg1: u32, arg2: u32) -> io::Result<usize> {
+    fn send_empty_packet(&mut self, cmd: u32, arg0: u32, arg1: u32, arg2: u32) -> io::Result<usize> {
         let args = &mut vec![0u32, 16];
         args[0] = arg0;
         args[1] = arg1;
@@ -74,7 +134,11 @@ impl NtrStream {
         self.send_packet(0, cmd, args, 0)
     }
 
-    pub fn tcp_stream(&self) -> &TcpStream {
+    fn tcp_stream(&mut self) -> &TcpStream {
         &self.tcp_stream
+    }
+
+    fn set_heartbeat_sendable(&self, val: bool) {
+        self.is_heartbeat_sendable.store(val, Ordering::SeqCst)
     }
 }
